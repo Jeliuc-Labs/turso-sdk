4c7349fe1725cd058d628595f695cdd8a9ea24fe
dmitrysufrai <dmitry.sufrai@jeliuc.com>
2024-03-16 05:26:51 +0200

    format

--- src/main/kotlin/serializer/LocalDateTimeSerializer.kt
+++ src/main/kotlin/serializer/LocalDateTimeSerializer.kt
@@ -5,13 +5,13 @@
 
 package com.jeliuc.turso.sdk.serializer
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = LocalDateTime::class)
